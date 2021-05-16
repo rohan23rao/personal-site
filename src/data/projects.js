624669c51506c5f3f79abbe0c810953c909f0dac
Rohan Rao <rohan23rao@gmail.com>
2021-05-16 23:21:22 +0000

    Changes to About ME

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,14 +1,14 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
+    title: 'Bias Detector',
+    subtitle: '2021 Lancer Hackathon',
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
     desc:
       'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
+      + 'Bias detector for detecting racial, political, or any kind of bias in text. '
+      + 'Used as a Sentiment analys '
       + 'the charity of your choice.',
   },
   {
