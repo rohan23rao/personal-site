6ec2e73c88cb29549801ad8e62be2339fa7c7486
Rohan Rao <rohan23rao@gmail.com>
2021-05-16 02:40:28 +0000

    Changes to skills and positions

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,16 +1,23 @@
 const degrees = [
   {
-    school: 'Stanford University',
-    degree: 'M.S. Computational and Mathematical Engineering',
-    link: 'https://stanford.edu',
+    school: 'Saint Francis High School',
+    degree: 'High School Diploma',
+    link: 'https://sfhs.com',
     year: 2016,
   },
-  {
-    school: 'University at Buffalo',
-    degree: 'B.S. Electrical Engineering, Computer Engineering',
-    link: 'https://buffalo.edu',
-    year: 2012,
-  },
 ];
-
+  // const degrees = [
+  //   {
+  //     school: 'Saint Francis High School',
+  //     degree: 'High School Diploma',
+  //     link: 'https://stanford.edu',
+  //     year: 2016,
+  //   },
+  //   {
+  //     school: 'University at Buffalo',
+  //     degree: 'B.S. Electrical Engineering, Computer Engineering',
+  //     link: 'https://buffalo.edu',
+  //     year: 2012,
+  //   },
+  // ];
 export default degrees;
