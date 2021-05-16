b9026e86b349ef0f3cd87a9a5cce37f79c8323cc
Rohan Rao <rohan23rao@gmail.com>
2021-05-16 23:51:39 +0000

    Changes to About

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,45 +3,14 @@ const data = [
   {
     title: 'Bias Detector',
     subtitle: '2021 Lancer Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    link: 'https://devpost.com/software/bias-checker-q25yrh',
+    image: '/images/projects/biasdetect.jpg',
+    date: '2021-02-27',
     desc:
-      'Built for a social impact hackathon. '
-      + 'Bias detector for detecting racial, political, or any kind of bias in text. '
-      + 'Used as a Sentiment analys '
-      + 'the charity of your choice.',
-  },
-  {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'Built a bias decetor for social impact at LancerHacks 2021 with my friend Shlok. '
+      + 'Bias detector for detecting racial, political, or other of bias in text. '
+      + 'Works as a Sentiment analyser. '
+      + 'Helps reduce bias in writing.',
   },
 ];
 
--- src/data/resume/courses.js
+++ src/data/resume/courses.js
@@ -1,63 +1,14 @@
 const courses = [
   {
-    title: 'Convex Optimization',
-    number: 'EE 364a',
-    link: 'http://stanford.edu/class/ee364a/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Machine Learning',
-    number: 'CS 229',
-    link: 'http://cs229.stanford.edu/',
+    title: 'Deep Learning Specialization',
+    number: 'DLS',
+    link: 'https://www.coursera.org/specializations/deep-learning',
     university: 'Stanford',
   },
   {
     title: 'Convolutional Neural Networks for Visual Recognition',
     number: 'CS 231n',
     link: 'http://cs231n.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Numerical Linear Algebra',
-    number: 'CME 302',
-    link:
-      'http://scpd.stanford.edu/search/publicCourseSearchDetails.do;jsessionid=561188A06434D7D97953C4706DE12831?method=load&courseId=11685',
-    university: 'Stanford',
-  },
-  {
-    title: 'Numerical Optimization',
-    number: 'CME 304',
-    link: 'http://web.stanford.edu/class/cme304/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Discrete Mathematics and Algorithms',
-    number: 'CME 305',
-    link: 'http://stanford.edu/~rezab/discrete/',
-    university: 'Stanford',
-  },
-  {
-    title: 'PDE’s of Applied Mathematics',
-    number: 'CME 303',
-    link: 'http://web.stanford.edu/class/math220/index.html',
-    university: 'Stanford',
-  },
-  {
-    title: 'Introduction to Linear Dynamical Systems',
-    number: 'EE 263',
-    link: 'http://ee263.stanford.edu/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Introduction to Statistical Signal Processing',
-    number: 'EE 278B',
-    link: 'http://web.stanford.edu/class/ee278/',
-    university: 'Stanford',
-  },
-  {
-    title: 'Spacecraft Design',
-    number: 'AA 236A',
-    link: '',
     university: 'Stanford',
   },
   {
